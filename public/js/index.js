e4146b05e6d96a79dca054465bd0773a529ff988
Asunder79 <isaac.gray30@yahoo.com>
2025-10-15 13:36:39 -0400

    added CORS+established our route. successful return codes observed.

--- public/js/index.js
+++ public/js/index.js
@@ -53,19 +53,47 @@
 //     }
 // }
 
+// async function getRandomImage() {
+//     const endpoint = "http://localhost:8080/api/v1/getRandomImage";
+//     try {
+//         const response = await fetch(endpoint);
+//         const returnedData = await response.json();
+//         const receivedPhotoUrl = returnedData.data;
+
+//         const imgDiv = document.querySelector(".background-img");
+//         imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+    // const client_id = "YOUR_API_KEY_HERE";
+    // const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    // try {
+    //     const response = await fetch(endpoint);
+    //     const returnedData = await response.json();
+    //     const receivedPhotoUrl = returnedData.urls.regular;
+
+    //     const imgDiv = document.querySelector(".background-img");
+    //     imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    // } catch (error) {
+    //     console.error(error);
+    // }
+// }
+
 async function getRandomImage() {
-    const client_id = "YOUR_API_KEY_HERE";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
 }
 
 getRandomImage();
+
+// module.exports = { getRandomImage };
